d406649f99e373315faa0c0d042417299330c15c
Md Moazz <moazzemh222@gmail.com>
2024-12-29 13:26:13 +0600

    2nd Update file

--- BestStoreMVC/BestStoreMVC/obj/Debug/net8.0/BestStoreMVC.AssemblyInfo.cs
+++ BestStoreMVC/BestStoreMVC/obj/Debug/net8.0/BestStoreMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BestStoreMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+005db1f328c3e9beb1a835dffdc5ab0d25e11340")]
 [assembly: System.Reflection.AssemblyProductAttribute("BestStoreMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BestStoreMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
